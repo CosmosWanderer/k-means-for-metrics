62630512a140e3ffe9d768d85bf6f6789e206b2b
Maxim <scourgetp@gmail.com>
2026-04-21 20:19:30 +0300

    [DEV] Basic version of k-means alg

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 import argparse as arg
 import os
+from sklearn.preprocessing import StandardScaler
+from sklearn.cluster import KMeans
+from kneed import KneeLocator
 
 # Parsing arguments (entering filename)
 
@@ -15,3 +18,34 @@ filename = args.filename
 
 filepath = "data/" + filename
 df = pd.read_csv(filepath)
+scaler = StandardScaler()
+X = scaler.fit_transform(df)
+
+# Finding optimal k using elbow method
+
+inertias = []
+k_range = range(1, 11)
+
+for k in k_range:
+    km = KMeans(n_clusters=k, random_state=42, n_init='auto')
+    km.fit(X)
+    inertias.append(km.inertia_)
+
+knee = KneeLocator(k_range, inertias, curve='convex', direction='decreasing')
+optimal_k = knee.elbow
+print(f"Optimal k = {optimal_k}")
+
+# Running k-means with optimal k
+
+km_final = KMeans(n_clusters=optimal_k, random_state=42, n_init='auto')
+df['cluster'] = km_final.fit_predict(X)
+
+# Saving clusters in different files
+
+os.makedirs("results", exist_ok=True)
+
+for cluster_id, group in df.groupby('cluster'):
+    group.drop(columns='cluster').to_csv(f"results/{filename[:-4]}_{cluster_id}.csv", index=False)
+
+print(f"Clusters data saved as 'results/{filename[:-4]}_id.csv'")
+
